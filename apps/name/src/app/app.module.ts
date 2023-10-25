a518b560b6f70d2f73e703c9d5fc0016c69c5da2
Tehilla Turin <56375130+t-turin@users.noreply.github.com>
2023-10-25 19:33:26 +0300

    Add code for angular-16.2.0 integrate-footer.
    [Url: razroo.com/community/angular-16.2.0/footer/integrate-footer]

--- apps/name/src/app/app.module.ts
+++ apps/name/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GlobalFooterModule } from "@name/common/ui";
+import { GlobalFooterModule } from "@name/common/ui";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GlobalFooterModule],
+  imports: [BrowserModule, GlobalFooterModule, GlobalFooterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
