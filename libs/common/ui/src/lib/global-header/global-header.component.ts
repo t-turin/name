caa5bfcbd406dc6b5c4504b678847e5bc60e7367
Tehilla Turin <56375130+t-turin@users.noreply.github.com>
2024-01-25 20:33:09 +0200

    Add code for angular-17.0.0 header-component.
    [Url: razroo.com/community/angular-17.0.0/header/header-component]

--- libs/common/ui/src/lib/global-header/global-header.component.ts
+++ libs/common/ui/src/lib/global-header/global-header.component.ts
@@ -7,7 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { faBell, faQuestionCircle, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'yellow-global-header',
+  selector: 'newRepo-global-header',
   templateUrl: './global-header.component.html',
   styleUrls: ['./global-header.component.scss'],
 })
