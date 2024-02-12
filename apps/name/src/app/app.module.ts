12d8c7107b44c1c9764bc16b0047b443fdf3454f
Tehilla Turin <56375130+t-turin@users.noreply.github.com>
2024-02-12 20:46:29 +0200

    Add code for angular-17.0.0 add-browser-animations-module.
    [Url: razroo.com/community/angular-17.0.0/custom-material-design-recipe/add-browser-animations-module]

--- apps/name/src/app/app.module.ts
+++ apps/name/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GlobalFooterModule } from "@name/common/ui";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GlobalFooterModule],
+  imports: [BrowserModule, GlobalFooterModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
